6ef8d0e6e932ce875855aeaa9966c438a37be981
Nattaphon <nattaphonnuayen@gmail.com>
2024-11-08 00:02:06 +0700

    decoration

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let value = 0;
 const handleLand = (url) => {
   landtext = document.querySelector(".text-land");
   landtext.textContent = url.name;
+  modalColor = document.querySelector(".modal-content");
+  modalColor.style.backgroundColor = url.color;
   modal.style.display = "block";
   setTimeout(() => {
     window.open(`https://www.youtube.com/watch?v=${url.videoId}`, "_blank");
@@ -16,10 +18,10 @@ var modal = document.getElementById("myModal");
 var span = document.getElementsByClassName("close")[0];
 
 const segmentVideo = {
-  1: { name: "เกมกระต่ายขาเดียว", videoId: "VTsMuhgBSSw" },
-  2: { name: "เกมไล่จับกบ", videoId: "yu2KwjXqSh8" },
-  3: { name: "การเล่นแปะแข็ง", videoId: "Qxx5WCxFaC4" },
-  4: { name: "เกมลิงชิงหาง", videoId: "hejyLXjc4qU" },
+  1: { name: "เกมกระต่ายขาเดียว", videoId: "VTsMuhgBSSw",color:"#F8C8DC" },
+  2: { name: "เกมไล่จับกบ", videoId: "yu2KwjXqSh8",color:"#B5EAD7" },
+  3: { name: "การเล่นแปะแข็ง", videoId: "Qxx5WCxFaC4",color:"#A7D8F8" },
+  4: { name: "เกมลิงชิงหาง", videoId: "hejyLXjc4qU",color:"#D9C2A3" },
 };
 
 spinBtn.addEventListener("click", () => {
